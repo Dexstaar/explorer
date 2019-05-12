d9bfcfc4dca24621572a0ecdded1d5f5ee2abeeb
dexstar <dexstaar@gmail.com>
2019-05-12 17:04:23 +0800

    working on test code implementation of DependencyExplorer

--- src/containers/DependencyExplorer.test.js
+++ src/containers/DependencyExplorer.test.js
@@ -1,34 +1,52 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import React from "react";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
-import { DependencyExplorer } from './DependencyExplorer';
-import dependencyReducer from '../reducers/dependencyReducer';
+import { DependencyExplorer } from "./DependencyExplorer";
+import dependencyReducer from "../reducers/dependencyReducer";
 
 let renderedDOM = null;
 const store = createStore(combineReducers({ dependencies: dependencyReducer }));
 let props = {};
 
-describe('DependencyExplorer Loader Test', () => {
-	it('renders correctly', () => {
+describe("DependencyExplorer Suggesstions Test", () => {
+  const suggesstionsMock = [
+    { name: "suggestion1" },
+    { name: "suggestion2" },
+    { name: "suggestion3" }
+  ];
+
+  it("renders correctly", () => {
     props = {
-        loadingSuggestions: fn => fn,
-        fetchSuggestions: fn => fn,
-        suggestions: []
+      loadingSuggestions: fn => fn,
+      fetchSuggestions: fn => fn,
+      suggestions: suggesstionsMock
     };
-    
+
     renderedDOM = mount(
       <Provider store={store}>
-			  <DependencyExplorer {...props} />
+        <DependencyExplorer {...props} />
       </Provider>
     );
-
-    
   });
-  
-  it('shows renderedDOM', () => {
-    console.log(renderedDOM.debug());
-    // expect(renderedDOM.find('Spinner').exists()).toBe(true);
+
+//   it("shows renderedDOM", () => {
+//     console.log(renderedDOM.debug());
+//   });
+
+  it("shows suggestions on Dropdown", () => {
+    // console.log(
+    //   renderedDOM
+    //     .find("DropdownItem")
+    //     .at(0)
+    //     .props()
+    // );
+    expect(
+      renderedDOM
+        .find("DropdownItem")
+        .at(0)
+        .props().value
+    ).toEqual(suggesstionsMock[0].name);
   });
 });
